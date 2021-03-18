c87d22caa10ba77069bf2c16fd1d63aebab27b8f
Cole Johnson <cburnjohnson@gmail.com>
2021-03-17 18:52:27 -0700

    navbar overlay

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Navbar() {
-  return <div>Navigation</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-layer"></div>Navigation
+    </div>
+  );
 }
 
 export default Navbar;
