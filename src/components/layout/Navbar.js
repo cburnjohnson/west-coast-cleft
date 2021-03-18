70b3826d2747462ed3658b758ed9893e00a1f3c6
Cole Johnson <cburnjohnson@gmail.com>
2021-03-17 19:16:11 -0700

    navbar styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="navbar-layer"></div>Navigation
+      <div className="navbar-layer"></div>
+      <div className="nav-logo">
+        WEST<span>COAST</span>
+        <span>CLEFT</span>
+      </div>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+      <div className="nav-contact">
+        <div>Westcoastcleft@gmail.com</div>
+        <div>541-408-1777</div>
+      </div>
     </div>
   );
 }
