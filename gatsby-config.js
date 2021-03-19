05983b0d1887d767b07bfc5f0bff41de85f4c60f
Cole Johnson <cburnjohnson@gmail.com>
2021-03-19 13:06:17 -0700

    removing unused files / updated meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `West Coast Cleft`,
+    description: `West Coast Cleft - Help care for and save any newborn puppy or kitten that is born with any congenital defects.`,
+    author: `@cburnjohnson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -33,4 +33,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
